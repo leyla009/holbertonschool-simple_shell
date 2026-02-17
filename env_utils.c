e82dfe7742a0f1875e371436807cd6e159db06f9
vitasec <davidabdurahmanov38@gmail.com>
2026-02-17 08:11:43 +0000

    fix

--- env_utils.c
+++ env_utils.c
@@ -1,14 +1,13 @@
 #include "main.h"
+#include <stdlib.h>
 
 char *env_memory_to_free = NULL;
-
 char **env_array_to_free = NULL;
 
 /**
  * _getenv - Gets the value of an environment variable.
  * @name: The name of the variable to find.
- *
- * Return: A pointer to the value within the environment string, or NULL.
+ * Return: A pointer to the value, or NULL.
  */
 char *_getenv(const char *name)
 {
@@ -17,94 +16,126 @@ char *_getenv(const char *name)
 
 	if (name == NULL || *name == '\0')
 		return (NULL);
-
 	name_len = _strlen(name);
-
 	while (environ[i])
 	{
 		if (_strncmp(environ[i], name, name_len) == 0 &&
 		    environ[i][name_len] == '=')
-		{
 			return (environ[i] + name_len + 1);
-		}
 		i++;
 	}
 	return (NULL);
 }
 
 /**
+ * _make_env_copy - Creates a deep copy of environ to allow freeing.
+ * Return: 0 on success, -1 on failure.
+ */
+int _make_env_copy(void)
+{
+	int i = 0, j;
+	char **new_env;
+
+	if (env_array_to_free) /* Already copied */
+		return (0);
+
+	while (environ[i])
+		i++;
+
+	new_env = malloc(sizeof(char *) * (i + 1));
+	if (!new_env)
+		return (-1);
+
+	for (j = 0; j < i; j++)
+	{
+		new_env[j] = malloc(_strlen(environ[j]) + 1);
+		if (!new_env[j])
+		{
+			/* Clean up partial allocation if needed */
+			return (-1);
+		}
+		_strcpy(new_env[j], environ[j]);
+	}
+	new_env[j] = NULL;
+	environ = new_env;
+	env_array_to_free = new_env; /* Mark for cleanup */
+	return (0);
+}
+
+/**
  * _setenv - Initialize a new environment variable or modify an existing one.
  * @name: Variable name.
  * @value: Variable value.
- *
  * Return: 0 on success, -1 on failure.
  */
 int _setenv(const char *name, const char *value)
 {
-	char *new_var, **new_environ;
-	int i = 0, j;
-	size_t name_len = _strlen(name), value_len = _strlen(value);
+	char *new_var, **new_env;
+	int i = 0, len;
 
-	new_var = malloc(name_len + value_len + 2);
-	if (!new_var) return (-1);
+	if (!name || !value)
+		return (-1);
+	if (_make_env_copy() == -1) /* Ensure we own the memory */
+		return (-1);
 
+	len = _strlen(name) + _strlen(value) + 2;
+	new_var = malloc(len);
+	if (!new_var)
+		return (-1);
 	_strcpy(new_var, (char *)name);
 	_strcat(new_var, "=");
 	_strcat(new_var, (char *)value);
-	
+
 	while (environ[i])
 	{
-		if (_strncmp(environ[i], name, name_len) == 0 && environ[i][name_len] == '=')
+		if (_strncmp(environ[i], name, _strlen(name)) == 0 &&
+		    environ[i][_strlen(name)] == '=')
 		{
+			free(environ[i]); /* Safe to free now */
 			environ[i] = new_var;
 			return (0);
 		}
 		i++;
 	}
-	
-	new_environ = malloc(sizeof(char *) * (i + 2));
-	if (!new_environ)
+	/* Add new variable */
+	new_env = malloc(sizeof(char *) * (i + 2));
+	if (!new_env)
 	{
 		free(new_var);
 		return (-1);
 	}
-
-	
-	for (j = 0; j < i; j++)
-		new_environ[j] = environ[j];
-	
-	new_environ[i] = new_var;
-	new_environ[i + 1] = NULL;
-	
-	environ = new_environ;
+	for (i = 0; environ[i]; i++)
+		new_env[i] = environ[i];
+	new_env[i] = new_var;
+	new_env[i + 1] = NULL;
+	free(env_array_to_free); /* Free old array backbone */
+	environ = new_env;
+	env_array_to_free = new_env;
 	return (0);
 }
 
 /**
  * _unsetenv - Remove an environment variable.
  * @name: The variable name to remove.
- *
  * Return: 0 on success, -1 on failure.
  */
 int _unsetenv(const char *name)
 {
 	int i = 0, j;
-	size_t name_len;
+	size_t len;
 
-	if (!name || *name == '\0' || _strchr(name, '=') != NULL)
-		return (-1);
-
-	name_len = _strlen(name);
-
+	if (!name || !env_array_to_free) /* Cannot unset OS env or empty */
+	{
+		if (_make_env_copy() == -1) return (-1);
+	}
+	len = _strlen(name);
 	while (environ[i])
 	{
-		if (_strncmp(environ[i], name, name_len) == 0 &&
-		    environ[i][name_len] == '=')
+		if (_strncmp(environ[i], name, len) == 0 && environ[i][len] == '=')
 		{
+			free(environ[i]);
 			for (j = i; environ[j]; j++)
-			{
 				environ[j] = environ[j + 1];
-			}
 			return (0);
 		}
 		i++;
--- main.c
+++ main.c
@@ -1,65 +1,56 @@
 #include "main.h"
 
 extern char *env_memory_to_free;
+extern char **env_array_to_free;
+
+void cleanup_all(char *line, char **argv);
 
 /**
  * main - Entry point for the simple shell
- *
  * Return: The last exit status
  */
-void cleanup_all(char *line, char **argv);
-
 int main(void)
 {
-    char *line = NULL, *token, *full_path = NULL;
-    size_t len = 0;
-    ssize_t nread;
-    char **argv;
-    int i, j, status = 0;
+	char *line = NULL, *token, *full_path = NULL;
+	size_t len = 0;
+	ssize_t nread;
+	char **argv;
+	int i, j, status = 0, exit_code;
 
-    while (1)
-    {
-	    argv = malloc(sizeof(char *) * 32);
-	    
-	    if (argv == NULL)
-		    break;
-
-	    if (isatty(STDIN_FILENO))
-            printf(":) ");
-	    
-	    nread = _getline(&line, &len, stdin);
-	    if (nread == -1)
-	    {
-		    if (isatty(STDIN_FILENO))
-			    printf("\n");
-		    
-		    cleanup_all(line, argv);
-		    return (status);
-	    }
-
-	    if (nread > 0 && line[nread - 1] == '\n')
-		    line[nread - 1] = '\0';
-
-	/* Tokenization */
-        i = 0;
-        token = _strtok(line, " \t\n\r");
-        while (token != NULL && i < 31)
-        {
-            argv[i++] = token;
-            token = _strtok(NULL, " \t\n\r");
-        }
-        argv[i] = NULL;
-
-        if (argv[0] == NULL)
+	while (1)
 	{
-		goto skip_execution;
-	}
-
-        /* 1. Builtin: Exit */
-        if (_strcmp(argv[0], "exit") == 0)
+		argv = malloc(sizeof(char *) * 32);
+		if (argv == NULL)
+			break;
+		if (isatty(STDIN_FILENO))
+			_puts(":) ");
+		
+		nread = _getline(&line, &len, stdin);
+		if (nread == -1)
 		{
-			int exit_code = status;
+			if (isatty(STDIN_FILENO))
+				_putchar('\n');
+			cleanup_all(line, argv);
+			return (status);
+		}
+		if (nread > 0 && line[nread - 1] == '\n')
+			line[nread - 1] = '\0';
 
+		i = 0;
+		token = _strtok(line, " \t\n\r");
+		while (token != NULL && i < 31)
+		{
+			argv[i++] = token;
+			token = _strtok(NULL, " \t\n\r");
+		}
+		argv[i] = NULL;
+
+		if (argv[0] == NULL)
+			goto skip_execution;
+
+		if (_strcmp(argv[0], "exit") == 0)
+		{
+			exit_code = status;
 			if (argv[1] != NULL)
 			{
 				for (j = 0; argv[1][j]; j++)
@@ -74,93 +65,86 @@ int main(void)
 				exit_code = _atoi(argv[1]);
 			}
 			cleanup_all(line, argv);
-			
-			(void)exit_code;
-
 			exit(exit_code);
 		}
-		
-        /* 2. Builtin: Env */
-        if (_strcmp(argv[0], "env") == 0)
-        {
-		print_env();
-		status = 0;
-		goto skip_execution;
-        }
+		if (_strcmp(argv[0], "env") == 0)
+		{
+			print_env();
+			status = 0;
+			goto skip_execution;
+		}
+		if (_strcmp(argv[0], "setenv") == 0)
+		{
+			if (argv[1] && argv[2])
+			{
+				if (_setenv(argv[1], argv[2]) == -1)
+					fprintf(stderr, "setenv: Error occurred\n");
+				else
+					status = 0;
+			}
+			else
+			{
+				fprintf(stderr, "setenv: usage: setenv VAR VALUE\n");
+				status = 1;
+			}
+			goto skip_execution;
+		}
+		if (_strcmp(argv[0], "unsetenv") == 0)
+		{
+			if (argv[1])
+				status = _unsetenv(argv[1]);
+			else
+			{
+				fprintf(stderr, "unsetenv: Too few arguments\n");
+				status = 1;
+			}
+			goto skip_execution;
+		}
+		if (_strcmp(argv[0], "cd") == 0)
+		{
+			status = shell_cd(argv);
+			goto skip_execution;
+		}
+		full_path = find_path(argv[0]);
+		if (full_path != NULL)
+		{
+			status = execute_command(full_path, argv);
+			if (full_path != argv[0])
+				free(full_path);
+		}
+		else
+		{
+			fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
+			status = 127;
+		}
 
-        /* 3. Builtin: Setenv */
-        if (_strcmp(argv[0], "setenv") == 0)
-        {
-            if (argv[1] && argv[2])
-            {
-                if (_setenv(argv[1], argv[2]) == -1)
-                    fprintf(stderr, "setenv: Error occurred\n");
-                else
-                    status = 0;
-            }
-            else
-            {
-                fprintf(stderr, "setenv: usage: setenv VAR VALUE\n");
-                status = 1;
-            }
-	    goto skip_execution;
-        }
-
-        /* 4. Builtin: Unsetenv */
-        if (_strcmp(argv[0], "unsetenv") == 0)
-        {
-            if (argv[1])
-                status = _unsetenv(argv[1]);
-            else
-            {
-                fprintf(stderr, "unsetenv: Too few arguments\n");
-                status = 1;
-            }
-            goto skip_execution;
-        }
-
-	/* 5. Builtin: CD  */
-	if (_strcmp(argv[0], "cd") == 0)
-	{
-		status = shell_cd(argv);
-		goto skip_execution;
+	skip_execution:
+		free(argv);
 	}
-        /* 6. Execution Logic (External Commands)  */
-        full_path = find_path(argv[0]);
-        if (full_path != NULL)
-        {
-            status = execute_command(full_path, argv);
-            if (full_path != argv[0])
-	    {
-		    free(full_path);
-	    }
-        }
-        else
-        {
-            fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-            status = 127;
-        }
-
-    skip_execution:
-	free(argv);
-    }
-    
-    return (status);
+	return (status);
 }
+
+/**
+ * cleanup_all - Frees all allocated memory
+ * @line: Input line buffer
+ * @argv: Arguments array
+ */
 void cleanup_all(char *line, char **argv)
 {
-    extern char *env_memory_to_free;
-    extern char **env_array_to_free;
-    
-    if (argv)
-        free(argv);
-    
-    if (line)
-        free(line);
-    
-    if (env_memory_to_free)
-	    free(env_memory_to_free);
+	int i;
 
-    if (env_array_to_free)
-	    free(env_array_to_free);
+	if (argv)
+		free(argv);
+	if (line)
+		free(line);
+	if (env_memory_to_free)
+		free(env_memory_to_free);
+	
+	/* Təmizləmə kodu yeniləndi: Massivin içindəki sətirləri də silir */
+	if (env_array_to_free)
+	{
+		for (i = 0; env_array_to_free[i]; i++)
+			free(env_array_to_free[i]);
+		free(env_array_to_free);
+	}
 }
