c2f8fba9e4a9ee706d93933235bda895eee5502f
Leyla <Leyla.khaspolad@gmail.com>
2026-02-17 00:07:58 +0000

    fix(cd): eliminate use-after-free via early environment snapshot
    Implemented strdup for all initial environment lookups in shell_cd to ensure pointer validity during subsequent _setenv operations.

--- builtins.c
+++ builtins.c
@@ -6,25 +6,23 @@ extern char **environ;
 /**
  * shell_cd - Changes the current working directory.
  * @argv: Array of arguments (argv[0] is "cd").
+ * 
  * Return: 0 on success, -1 on failure.
  */
 
 int shell_cd(char **argv)
 {
     char *target = NULL, cwd[1024], *target_copy = NULL;
-    char *home_raw = _getenv("HOME");
-    char *old_raw = _getenv("OLDPWD");
-    char *pwd_raw = _getenv("PWD");
 
-    char *home = home_raw ? _strdup(home_raw) : NULL;
-    char *oldpwd = old_raw ? _strdup(old_raw) : NULL;
-    char *pwd = pwd_raw ? _strdup(pwd_raw) : NULL;
+    char *s_home = _getenv("HOME") ? _strdup(_getenv("HOME")) : NULL;
+    char *s_old = _getenv("OLDPWD") ? _strdup(_getenv("OLDPWD")) : NULL;
+    char *s_pwd = _getenv("PWD") ? _strdup(_getenv("PWD")) : NULL;
 
     if (!argv[1])
-        target = home;
+        target = s_home;
     else if (_strcmp(argv[1], "-") == 0)
     {
-        target = oldpwd ? oldpwd : pwd;
+        target = s_old ? s_old : s_pwd;
         if (target)
         {
             _puts(target);
@@ -35,25 +33,24 @@ int shell_cd(char **argv)
         target = argv[1];
 
     if (target)
-        target_copy = _strdup(target);
-
-    if (target_copy && getcwd(cwd, sizeof(cwd)))
     {
-        if (chdir(target_copy) != 0)
+        target_copy = _strdup(target);
+        if (target_copy && getcwd(cwd, sizeof(cwd)))
         {
-            fprintf(stderr, "./hsh: 1: cd: can't cd to %s\n", target_copy);
-        }
-        else
-        {
-            _setenv("OLDPWD", cwd);
-            if (getcwd(cwd, sizeof(cwd)))
-                _setenv("PWD", cwd);
+            if (chdir(target_copy) != 0)
+                fprintf(stderr, "./hsh: 1: cd: can't cd to %s\n", target_copy);
+            else
+            {
+                _setenv("OLDPWD", cwd);
+                if (getcwd(cwd, sizeof(cwd)))
+                    _setenv("PWD", cwd);
+            }
         }
     }
 
-    free(home);
-    free(oldpwd);
-    free(pwd);
+    free(s_home);
+    free(s_old);
+    free(s_pwd);
     free(target_copy);
     return (0);
 }
