0e06745cefcb41a90be6fdd5e0baa70948c7a275
Leyla <Leyla.khaspolad@gmail.com>
2026-02-17 00:30:34 +0000

    fix(env): synchronize environment pointer safety
    Isolated environment lookups to prevent race conditions during array reallocation and ensured all temporary captures are heap-allocated.

--- builtins.c
+++ builtins.c
@@ -13,51 +13,40 @@ extern char **environ;
 int shell_cd(char **argv)
 {
     char *target = NULL, cwd[1024], *target_copy = NULL;
-
-    char *h_raw = _getenv("HOME");
-    char *o_raw = _getenv("OLDPWD");
-    char *p_raw = _getenv("PWD");
-
-    char *home = h_raw ? _strdup(h_raw) : NULL;
-    char *oldpwd = o_raw ? _strdup(o_raw) : NULL;
-    char *pwd = p_raw ? _strdup(p_raw) : NULL;
+    char *old_val = _getenv("OLDPWD");
+    char *s_old = old_val ? _strdup(old_val) : NULL;
+    char *s_home = _getenv("HOME") ? _strdup(_getenv("HOME")) : NULL;
 
     if (!argv[1])
-        target = home;
+        target = s_home;
     else if (_strcmp(argv[1], "-") == 0)
-    {
-        target = oldpwd ? oldpwd : pwd;
-        if (target)
-        {
-            _puts(target);
-            _putchar('\n');
-        }
-    }
+        target = s_old;
     else
         target = argv[1];
 
-    if (target)
-    {
-        target_copy = _strdup(target);
-        if (target_copy && getcwd(cwd, sizeof(cwd)))
-        {
-            if (chdir(target_copy) != 0)
-                fprintf(stderr, "./hsh: 1: cd: can't cd to %s\n", target_copy);
-            else
-            {
-                _setenv("OLDPWD", cwd);
-                if (getcwd(cwd, sizeof(cwd)))
-                    _setenv("PWD", cwd);
-            }
-        }
+    if (!target) {
+        free(s_old); free(s_home);
+        return (0);
     }
 
-    free(home);
-    free(oldpwd);
-    free(pwd);
-    free(target_copy);
+    target_copy = _strdup(target);
+    if (getcwd(cwd, sizeof(cwd)) && target_copy)
+    {
+        if (chdir(target_copy) == 0)
+        {
+            _setenv("OLDPWD", cwd); 
+            if (getcwd(cwd, sizeof(cwd)))
+                _setenv("PWD", cwd);
+        }
+        else
+            fprintf(stderr, "./hsh: 1: cd: can't cd to %s\n", target_copy);
+    }
+
+    free(target_copy); free(s_old); free(s_home);
     return (0);
 }
+
+
 /**
  * print_env - prints the environment.
  *
