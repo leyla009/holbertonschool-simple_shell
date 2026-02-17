39b8a1dec0b9eb65b3835e962eb90c6b771272cb
Leyla <Leyla.khaspolad@gmail.com>
2026-02-17 00:40:01 +0000

    fix(cd): tighten memory safety and environment sync
    Refactored shell_cd to ensure all environment snapshots are freed only at function exit and matched error output formatting for jail compatibility.

--- builtins.c
+++ builtins.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 
 extern char **environ;
@@ -6,23 +7,22 @@ extern char **environ;
 /**
  * shell_cd - Changes the current working directory.
  * @argv: Array of arguments (argv[0] is "cd").
- * 
- * Return: 0 on success, -1 on failure.
+ * * Return: 0 on success.
  */
-
 int shell_cd(char **argv)
 {
     char *target = NULL, cwd[1024], *target_copy = NULL;
-    char *h_raw = _getenv("HOME"), *o_raw = _getenv("OLDPWD"), *p_raw = _getenv("PWD");
-    char *home = h_raw ? _strdup(h_raw) : NULL;
-    char *oldpwd = o_raw ? _strdup(o_raw) : NULL;
-    char *pwd = p_raw ? _strdup(p_raw) : NULL;
+    char *h_val = _getenv("HOME"), *o_val = _getenv("OLDPWD"), *p_val = _getenv("PWD");
 
-    if (!argv[1])
-        target = home;
+    char *s_home = h_val ? _strdup(h_val) : NULL;
+    char *s_old = o_val ? _strdup(o_val) : NULL;
+    char *s_pwd = p_val ? _strdup(p_val) : NULL;
+
+    if (!argv[1] || _strcmp(argv[1], "$HOME") == 0)
+        target = s_home;
     else if (_strcmp(argv[1], "-") == 0)
     {
-        target = oldpwd ? oldpwd : pwd;
+        target = s_old ? s_old : s_pwd;
         if (target)
         {
             _puts(target);
@@ -37,21 +37,25 @@ int shell_cd(char **argv)
         target_copy = _strdup(target);
         if (target_copy && getcwd(cwd, sizeof(cwd)))
         {
-            if (chdir(target_copy) != 0)
-                fprintf(stderr, "./hsh: 1: cd: can't cd to %s\n", target_copy);
-            else
+            if (chdir(target_copy) == 0)
             {
                 _setenv("OLDPWD", cwd);
                 if (getcwd(cwd, sizeof(cwd)))
                     _setenv("PWD", cwd);
             }
+            else
+            {
+                fprintf(stderr, "./hsh: 1: cd: can't cd to %s\n", target_copy);
+            }
         }
+        free(target_copy);
     }
 
-    free(home); free(oldpwd); free(pwd); free(target_copy);
+    free(s_home);
+    free(s_old);
+    free(s_pwd);
     return (0);
 }
-
 /**
  * print_env - prints the environment.
  *
