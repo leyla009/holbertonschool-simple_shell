479ee8135d7e12f24adfa7adae325be93417255d
vitasec <davidabdurahmanov38@gmail.com>
2026-02-17 08:50:38 +0000

    change main.c and buitlin.c

--- builtins.c
+++ builtins.c
@@ -1,76 +1,50 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 
 /**
- * shell_cd - Changes the current working directory.
- * @argv: Array of arguments (argv[0] is "cd").
- * Return: 0 on success, 1 on failure.
+ * shell_cd - Changes the current directory of the process
+ * @argv: Arguments array (argv[1] is the directory)
+ * Return: 0 on success, 1 on failure
  */
 int shell_cd(char **argv)
 {
-	char *dir, buffer[1024];
-	int ret;
+	char *dir;
+	char buffer[1024];
 	char *cwd;
 
-	cwd = getcwd(buffer, 1024);
-	if (!cwd)
-		return (-1);
-
-	if (argv[1] == NULL) /* Case: cd (arqumentsiz) */
+	if (argv[1] == NULL)
 	{
 		dir = _getenv("HOME");
 		if (!dir)
-		{
-			/* HƏLL: Əgər HOME yoxdursa, heç nə etmə, return 0 qaytar */
-			return (0);
-		}
+			dir = _getenv("PWD");
 	}
-	else if (_strcmp(argv[1], "-") == 0) /* Case: cd - */
+	else if (_strcmp(argv[1], "-") == 0)
 	{
 		dir = _getenv("OLDPWD");
 		if (!dir)
 		{
-			_puts(cwd);
-			_putchar('\n');
+			_puts(getcwd(buffer, 1024));
 			return (0);
 		}
 		_puts(dir);
-		_putchar('\n');
 	}
-	else /* Case: cd /path */
+	else
 	{
 		dir = argv[1];
 	}
 
-	ret = chdir(dir);
-
-	if (ret == -1)
+	if (chdir(dir) == -1)
 	{
 		fprintf(stderr, "./hsh: 1: cd: can't cd to %s\n", argv[1]);
-		return (0);
+		return (2); /* Error status for cd failure */
 	}
-	else
+
+	/* Update PWD and OLDPWD */
+	cwd = getcwd(buffer, 1024);
+	if (cwd)
 	{
-		_setenv("OLDPWD", cwd);
-		_setenv("PWD", getcwd(buffer, 1024));
+		_setenv("OLDPWD", _getenv("PWD"));
+		_setenv("PWD", cwd);
 	}
+	
 	return (0);
-}
-
-/**
- * print_env - prints the environment.
- * Return: void.
- */
-void print_env(void)
-{
-	int i = 0;
-
-	while (environ[i])
-	{
-		_puts(environ[i]);
-		_putchar('\n');
-		i++;
-	}
 }
--- main.c
+++ main.c
@@ -1,7 +1,9 @@
 #include "main.h"
 
-extern char *env_memory_to_free;
-extern char **env_array_to_free;
+/* --- GLOBAL VARIABLE DEFINITIONS --- */
+/* Bu sətirlər mütləq olmalıdır ki, linker xəta verməsin */
+char *env_memory_to_free = NULL;
+char **env_array_to_free = NULL;
 
 void cleanup_all(char *line, char **argv);
 int execute_segment(char *command, int *status);
@@ -20,15 +22,13 @@ int main(int argc, char **argv)
 	ssize_t nread;
 	int status = 0;
 	int i;
-	int fd = STDIN_FILENO; /* Default input is stdin */
+	int fd = STDIN_FILENO; 
 
-	/* FILE MODE: Əgər arqument varsa, faylı açmağa çalışırıq */
 	if (argc == 2)
 	{
 		fd = open(argv[1], O_RDONLY);
 		if (fd == -1)
 		{
-			/* Checker-in tələb etdiyi dəqiq xəta mesajı */
 			fprintf(stderr, "%s: 0: Can't open %s\n", argv[0], argv[1]);
 			return (127);
 		}
@@ -36,11 +36,10 @@ int main(int argc, char **argv)
 
 	while (1)
 	{
-		/* Prompt yalnız interaktiv rejimdə və fayldan oxumuruqsa çıxmalıdır */
 		if (isatty(STDIN_FILENO) && fd == STDIN_FILENO)
 			_puts(":) ");
 
-		nread = _getline(&line, &len, fd); /* fd ötürülür */
+		nread = _getline(&line, &len, fd);
 		if (nread == -1)
 		{
 			if (isatty(STDIN_FILENO) && fd == STDIN_FILENO)
@@ -55,7 +54,6 @@ int main(int argc, char **argv)
 		if (nread > 0 && line[nread - 1] == '\n')
 			line[nread - 1] = '\0';
 
-		/* Semicolon handling */
 		cmd_start = line;
 		for (i = 0; line[i] != '\0'; i++)
 		{
@@ -75,11 +73,6 @@ int main(int argc, char **argv)
 	return (status);
 }
 
-/**
- * process_logical - Handles && and || operators
- * @line: The command line segment
- * @status: Pointer to the last status
- */
 void process_logical(char *line, int *status)
 {
 	char *start = line;
@@ -107,166 +100,3 @@ void process_logical(char *line, int *status)
 				start[i] = '\0';
 				next = start + i + 2;
 				break;
-			}
-		}
-
-		if (prev_op == 1 && *status != 0)
-		{
-			/* Skip logic */
-		}
-		else if (prev_op == 2 && *status == 0)
-		{
-			/* Skip logic */
-		}
-		else
-		{
-			execute_segment(start, status);
-		}
-
-		start = next;
-		prev_op = op;
-	}
-}
-
-/**
- * execute_segment - Parses and executes a single command part
- * @command: The command string
- * @status: Pointer to status
- * Return: 0
- */
-int execute_segment(char *command, int *status)
-{
-	char **argv;
-	char *token, *full_path = NULL;
-	int i, j, exit_code;
-
-	if (command == NULL || *command == '\0')
-		return (0);
-
-	argv = malloc(sizeof(char *) * 32);
-	if (argv == NULL)
-		return (-1);
-
-	i = 0;
-	token = _strtok(command, " \t\n\r");
-	while (token != NULL && i < 31)
-	{
-		argv[i++] = token;
-		token = _strtok(NULL, " \t\n\r");
-	}
-	argv[i] = NULL;
-
-	if (argv[0] == NULL)
-	{
-		free(argv);
-		return (0);
-	}
-
-	if (_strcmp(argv[0], "exit") == 0)
-	{
-		exit_code = *status;
-		if (argv[1] != NULL)
-		{
-			for (j = 0; argv[1][j]; j++)
-			{
-				if (argv[1][j] < '0' || argv[1][j] > '9')
-				{
-					fprintf(stderr, "./hsh: 1: exit: Illegal number: %s\n", argv[1]);
-					*status = 2;
-					free(argv);
-					return (0);
-				}
-			}
-			exit_code = _atoi(argv[1]);
-		}
-		free(argv);
-		cleanup_all(NULL, NULL);
-		if (env_memory_to_free) free(env_memory_to_free);
-		exit(exit_code);
-	}
-	if (_strcmp(argv[0], "env") == 0)
-	{
-		print_env();
-		*status = 0;
-		free(argv);
-		return (0);
-	}
-	if (_strcmp(argv[0], "setenv") == 0)
-	{
-		if (argv[1] && argv[2])
-		{
-			if (_setenv(argv[1], argv[2]) == -1)
-			{
-				fprintf(stderr, "setenv: Error occurred\n");
-				*status = -1;
-			}
-			else
-				*status = 0;
-		}
-		else
-		{
-			fprintf(stderr, "setenv: usage: setenv VAR VALUE\n");
-			*status = 1;
-		}
-		free(argv);
-		return (0);
-	}
-	if (_strcmp(argv[0], "unsetenv") == 0)
-	{
-		if (argv[1])
-			*status = _unsetenv(argv[1]);
-		else
-		{
-			fprintf(stderr, "unsetenv: Too few arguments\n");
-			*status = 1;
-		}
-		free(argv);
-		return (0);
-	}
-	if (_strcmp(argv[0], "cd") == 0)
-	{
-		*status = shell_cd(argv);
-		free(argv);
-		return (0);
-	}
-
-	full_path = find_path(argv[0]);
-	if (full_path != NULL)
-	{
-		*status = execute_command(full_path, argv);
-		if (full_path != argv[0])
-			free(full_path);
-	}
-	else
-	{
-		fprintf(stderr, "./hsh: 1: %s: not found\n", argv[0]);
-		*status = 127;
-	}
-
-	free(argv);
-	return (0);
-}
-
-/**
- * cleanup_all - Frees all allocated memory
- * @line: Input line buffer
- * @argv: Arguments array
- */
-void cleanup_all(char *line, char **argv)
-{
-	int i;
-
-	if (argv)
-		free(argv);
-	if (line)
-		free(line);
-	if (env_memory_to_free)
-		free(env_memory_to_free);
-
-	if (env_array_to_free)
-	{
-		for (i = 0; env_array_to_free[i]; i++)
-			free(env_array_to_free[i]);
-		free(env_array_to_free);
-	}
-}
